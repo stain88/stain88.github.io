b002c84950565790efde53220d8588b6a2da7a7e
stain88 <marcbaghdadi@gmail.com>
2015-12-28 16:46:31 +0000

    site outline done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
       anchors: ['landing', 'about', 'portfolio', 'contact', 'lastPage'],
+      sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
       menu: '#myMenu'
     });
 });
