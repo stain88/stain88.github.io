d7e6e74b0478c7be1adeb198d63e1b15042ff0e6
stain88 <marcbaghdadi@gmail.com>
2015-12-29 11:54:24 +0000

    changed bootstrap theme

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
       anchors: ['landing', 'about', 'portfolio', 'contact', 'lastPage'],
-      sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
       menu: '#myMenu'
     });
 });
